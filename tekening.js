9859cde9f8ba003c634f9ceaf7b73640c99b3d4a
soupskew <38813963+soupskew@users.noreply.github.com>
2018-06-27 09:54:15 +0200

    Create tekening.js

---
+++ tekening.js
@@ -0,0 +1,18 @@
+
+
+function setup (){
+createCanvas(3000,3000);
+
+
+}
+
+function draw(){
+
+mouseIsPressed && ellipse(mouseX, mouseY, 1,1);
+mouseIsPressed && fill(0);
+
+}
+
+function knopje() {
+    location.reload();
+}
